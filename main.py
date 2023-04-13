602a69341d556fe1d6fc8b3358c721c48cb27a4a
Darktrash <69591370+Darktrash@users.noreply.github.com>
2023-04-13 15:17:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import time
 import random
-from tkinter import *
+from tkinter import Tk
 from threading import Thread
 
 
 class Window:
     def __init__(self, size: str, timer: float, background_colors: tuple[str, ...]) -> None:
-        self.size = size.split("x")
+        self.size = size.split('x')
         self.timer = timer
         self.background_colors = background_colors
         self.root = Tk()
-        self.root.title("")
+        self.root.title('')
         self.root.geometry(size)
         self.root.resizable(False, False)
-        self.root.attributes("-topmost", True)
-        self.root.protocol("WM_DELETE_WINDOW", main)
+        self.root.attributes('-topmost', True)
+        self.root.protocol('WM_DELETE_WINDOW', main)
 
         Thread(target=self.animate_window).start() 
         Thread(target=self.create_new_window).start()
@@ -27,8 +27,8 @@ class Window:
 
         while True:
             self.root.deiconify()
-            self.root["bg"] = random.choice(self.background_colors)
-            self.root.geometry(f"+{random.choice(width_positions)}+{random.choice(height_positions)}")
+            self.root['bg'] = random.choice(self.background_colors)
+            self.root.geometry(f'+{random.choice(width_positions)}+{random.choice(height_positions)}')
             time.sleep(self.timer)
 
     def create_new_window(self) -> None:
@@ -37,9 +37,9 @@ class Window:
 
 
 def main():
-    rainbow_colors = ("#9400D3", "#4B0082", "#0000FF", "#00FF00", "#FFFF00", "#FF7F00", "#FF0000")
-    Window("300x250", 0.5, rainbow_colors)
+    rainbow_colors = ('#9400D3', '#4B0082', '#0000FF', '#00FF00', '#FFFF00', '#FF7F00', '#FF0000')
+    Window('300x250', 0.5, rainbow_colors)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
